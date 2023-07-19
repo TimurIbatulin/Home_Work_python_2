78a0fdbf33457f51759bf016b2c8c3f78c8d00fd
Тимур Ибатулин <timuribatulin@iMac-Timur-2.Dlink>
2023-07-19 19:49:21 +0300

    Задача 3на сдачу

--- Task_3.py
+++ Task_3.py
@@ -1,6 +1,9 @@
 # Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions
 
+import fractions
+
+
 first_fraction = str(input('Введите первую дробь в формате "13/57" - '))
 second_fraction = str(input('Введите вторую дробь в формате "13/57" - '))
 
@@ -25,3 +28,7 @@ for i in range(2, 9, 1):
 
 print(first_fraction, ' + ', second_fraction, ' = ', int(addition), '/', int(denominator))
 print(first_fraction, ' * ', second_fraction, ' = ', int(multiplication), '/', int(denominator_m))
+
+f1 = fractions.Fraction(int(first[0]), int(first[1]))
+f2 = fractions.Fraction(int(second[0]), int(second[1]))
+print('Проверка через модуль fraction ', 'Сложение = ', f1+f2, 'Умножение = ', f1+f2)
